f561352a06cfeb1633403c413609355fa56700b8
yogyrahmawan <yogy.frestarahmawan@gmail.com>
2019-09-30 14:31:59 +0700

    double linked list

--- linkedlist/target/debug/deps/linkedlist-5a79dd6949efcb5d.d
+++ linkedlist/target/debug/deps/linkedlist-5a79dd6949efcb5d.d
@@ -1,6 +1,7 @@
-/Volumes/hddstorage/Users/yogydev/Development/projects/zumata/rust/rust-datastructure/linkedlist/target/debug/deps/liblinkedlist-5a79dd6949efcb5d.rlib: src/lib.rs src/single_linked_list.rs
+/Volumes/hddstorage/Users/yogydev/Development/projects/zumata/rust/rust-datastructure/linkedlist/target/debug/deps/liblinkedlist-5a79dd6949efcb5d.rlib: src/lib.rs src/single_linked_list.rs src/double_linked_list.rs
 
-/Volumes/hddstorage/Users/yogydev/Development/projects/zumata/rust/rust-datastructure/linkedlist/target/debug/deps/linkedlist-5a79dd6949efcb5d.d: src/lib.rs src/single_linked_list.rs
+/Volumes/hddstorage/Users/yogydev/Development/projects/zumata/rust/rust-datastructure/linkedlist/target/debug/deps/linkedlist-5a79dd6949efcb5d.d: src/lib.rs src/single_linked_list.rs src/double_linked_list.rs
 
 src/lib.rs:
 src/single_linked_list.rs:
+src/double_linked_list.rs:
