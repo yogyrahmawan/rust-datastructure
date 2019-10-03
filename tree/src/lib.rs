5dafb62b34f84d3f7d007ee055d2b91987136b5d
yogyrahmawan <yogy.frestarahmawan@gmail.com>
2019-10-04 05:48:36 +0700

    initial work of tree

---
+++ tree/src/lib.rs
@@ -0,0 +1,17 @@
+
+#[derive(Clone, Debug)]
+pub struct IoTDevice {
+    pub numerical_id: u64,
+    pub path: String,
+    pub address: String,
+}
+
+impl IoTDevice {
+    pub fn new(id: u64, address: impl Into<String>, path: impl Into<String>) -> IoTDevice {
+        IoTDevice {
+            address: address.into(),
+            numerical_id: id,
+            path: path.into(),
+        }
+    }
+}
